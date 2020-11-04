07e8f4d7764c01671f15866027c7d0463c7d35ac
valen2510 <valenjaramillo1025@hotmail.com>
2020-11-04 02:32:14 +0000

    Create a program entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    Console module
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+        Define HBNBCommand class for interactive console
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """
+            Command to quit from the command interpreter
+        """
+        return True
+
+    def do_EOF(self, args):
+        """
+            Command to exit from the command interpreter
+        """
+        return True
+
+    def help_quit(self):
+        """
+            Command to show quit documentation
+        """
+        print("Quit command to exit the program\n")
+
+    def emptyline(self):
+        """
+            Command do nothing when emptyline appears
+        """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
